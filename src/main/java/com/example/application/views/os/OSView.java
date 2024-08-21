2fb9c51aee5e841428ef246ed5d589b124baba6d
sophiasussa <sophiasussa@outlook.com>
2024-08-21 14:52:50 -0300

    View de Ordem de Serviço

--- src/main/java/com/example/application/views/os/OSView.java
+++ src/main/java/com/example/application/views/os/OSView.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
 import com.vaadin.flow.component.html.Hr;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -20,7 +21,7 @@ import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
 import java.util.ArrayList;
 import java.util.List;
 
-@PageTitle("OS")
+@PageTitle("Ordem de Serviço")
 @Route(value = "my-view5", layout = MainLayout.class)
 public class OSView extends Composite<VerticalLayout> {
 
@@ -42,41 +43,52 @@ public class OSView extends Composite<VerticalLayout> {
         TextField textField3 = new TextField();
         Button buttonPrimary2 = new Button();
         Hr hr2 = new Hr();
+        VerticalLayout layout = new VerticalLayout();
         getContent().setWidth("100%");
         getContent().getStyle().set("flex-grow", "1");
         formLayout3Col.setWidth("100%");
         formLayout3Col.setResponsiveSteps(new ResponsiveStep("0", 1), new ResponsiveStep("250px", 2),
                 new ResponsiveStep("500px", 3));
-        comboBox.setLabel("Combo Box");
+        comboBox.setPlaceholder("Cliente");
         comboBox.setWidth("min-content");
+        comboBox.addClassName("rounded-text-field");
         setComboBoxSampleData(comboBox);
-        comboBox2.setLabel("Combo Box");
+        comboBox2.setPlaceholder("Veículo");
         comboBox2.setWidth("min-content");
+        comboBox2.addClassName("rounded-text-field");
         setComboBoxSampleData(comboBox2);
-        comboBox3.setLabel("Combo Box");
+        comboBox3.setPlaceholder("Mecânico");
         comboBox3.setWidth("min-content");
+        comboBox3.addClassName("rounded-text-field");
         setComboBoxSampleData(comboBox3);
-        comboBox4.setLabel("Combo Box");
+        comboBox4.setPlaceholder("Serviços");
         comboBox4.setWidth("min-content");
+        comboBox4.addClassName("rounded-text-field");
         setComboBoxSampleData(comboBox4);
-        comboBox5.setLabel("Combo Box");
+        comboBox5.setPlaceholder("Peças");
         comboBox5.setWidth("min-content");
+        comboBox5.addClassName("rounded-text-field");
         setComboBoxSampleData(comboBox5);
-        textField.setLabel("Text field");
+        textField.setPlaceholder("Número da OS");
+        textField.addClassName("rounded-text-field");
         textField.setWidth("min-content");
-        textField2.setLabel("Text field");
+        textField2.setPlaceholder("Valor Total");
         textField2.setWidth("min-content");
-        datePicker.setLabel("Date picker");
+        textField2.addClassName("rounded-text-field");
+        datePicker.setPlaceholder("Data de Abertura");
         datePicker.setWidth("min-content");
-        datePicker2.setLabel("Date picker");
+        datePicker.addClassName("rounded-text-field");
+        datePicker2.setPlaceholder("Data de Encerramento");
         datePicker2.setWidth("min-content");
+        datePicker2.addClassName("rounded-text-field");
         layoutColumn2.setWidthFull();
         getContent().setFlexGrow(1.0, layoutColumn2);
         layoutColumn2.setWidth("100%");
         layoutColumn2.getStyle().set("flex-grow", "1");
         layoutColumn2.setJustifyContentMode(JustifyContentMode.START);
         layoutColumn2.setAlignItems(Alignment.END);
-        buttonPrimary.setText("Button");
+        buttonPrimary.setText("Salvar");
+        buttonPrimary.getStyle().set("border-radius", "25px");
         buttonPrimary.setWidth("min-content");
         buttonPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         layoutRow.setWidthFull();
@@ -86,12 +98,15 @@ public class OSView extends Composite<VerticalLayout> {
         layoutRow.setHeight("70px");
         layoutRow.setAlignItems(Alignment.END);
         layoutRow.setJustifyContentMode(JustifyContentMode.END);
-        textField3.setLabel("Text field");
+        textField3.setPlaceholder("Pesquisar");
+        textField3.addClassName("rounded-text-field");
         textField3.setWidth("min-content");
-        buttonPrimary2.setText("Button");
+        hr.getStyle().set("box-shadow", "0 1px 4px rgba(0, 0, 0, 0.2)");
+        hr2.getStyle().set("box-shadow", "0 -1px 4px rgba(0, 0, 0, 0.2)");
+        buttonPrimary2.setIcon(VaadinIcon.SEARCH.create());
+        buttonPrimary2.getStyle().set("border-radius", "50%");
         buttonPrimary2.setWidth("min-content");
         buttonPrimary2.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        getContent().add(formLayout3Col);
         formLayout3Col.add(comboBox);
         formLayout3Col.add(comboBox2);
         formLayout3Col.add(comboBox3);
@@ -101,6 +116,8 @@ public class OSView extends Composite<VerticalLayout> {
         formLayout3Col.add(textField2);
         formLayout3Col.add(datePicker);
         formLayout3Col.add(datePicker2);
+        layout.add(formLayout3Col);
+        getContent().add(layout);
         getContent().add(layoutColumn2);
         layoutColumn2.add(buttonPrimary);
         layoutColumn2.add(hr);
